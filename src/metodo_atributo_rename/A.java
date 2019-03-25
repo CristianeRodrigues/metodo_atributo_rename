dd1729359dae08c1e54e820613787bcf47e329ef
cristiane <cris_csr@yahoo.com.br>
2019-03-25 15:25:51 -0300

    first commit

---
+++ src/metodo_atributo_rename/A.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodo_atributo_rename;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class A {
+    int codigoA;
+    
+   public int getCodigoA() {
+        return this.codigoA;
+    }
+
+    public void setCodigoA(int codigoA) {
+        this.codigoA = codigoA;
+    }  
+}
---
+++ src/metodo_atributo_rename/B.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodo_atributo_rename;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class B {
+    int codigoB;
+    
+   public int getCodigoB() {
+        return this.codigoB;
+    }
+
+    public void setCodigoB(int codigoB) {
+        this.codigoB = codigoB;
+    }  
+}
