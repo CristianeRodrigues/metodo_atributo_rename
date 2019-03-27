3b7da5e6f6a17873d95c6de764dc8da6c9d5f7b0
cristiane <cris_csr@yahoo.com.br>
2019-03-27 05:35:33 -0300

    add classe C

---
+++ src/metodo_atributo_rename/C.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodo_atributo_rename;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class C {
+    float codigoC;
+    A ClassA;
+    
+   public float getCodigoC() {
+       System.out.println("codigo c");
+        return this.codigoC;
+    }
+
+    public void setCodigoC(float codigoC) {
+        this.codigoC = codigoC;
+    }  
+    
+}
